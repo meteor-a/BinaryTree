3d1a060a5b743d01b761fbcfc1b5ee1b0da81134
meteor-a <61750320+meteor-a@users.noreply.github.com>
2021-11-15 07:55:10 +0300

    graph

--- binary_tree/binary_tree.cpp
+++ binary_tree/binary_tree.cpp
@@ -1,6 +1,7 @@
 #include "binary_tree.h"
 #include "assert.h"
 #include "malloc.h"
+#include <stdio.h>
 
 /*----------------------------------------------------------------------------------------------*/
 
@@ -9,13 +10,14 @@ const int _ERROR_CODE_SUCCESSFULL__ = 0;
 /*----------------------------------------------------------------------------------------------*/
 
 int _InizializeNode__(Node* node);
+int _PrintfGraph__(Node* node, int num_node_left, int num_node_right, FILE* graph_file, int side);
 
 /*----------------------------------------------------------------------------------------------*/
 
-int _BTreeConstructor__(Btree* b_tree DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_call)) {
+int _BTreeConstructor__(BTree* b_tree DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_call)) {
     assert(b_tree != nullptr);
 
-    b_tree->head = (Node*) calloc(1, sizeof(Node*));
+    b_tree->head = (Node*)calloc(1, sizeof(Node));
     assert(b_tree->head != nullptr);
 
     _InizializeNode__(b_tree->head);
@@ -23,7 +25,7 @@ int _BTreeConstructor__(Btree* b_tree DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT_
     return _ERROR_CODE_SUCCESSFULL__;
 }
 
-int _BTreeDestructor__(Btree* b_tree DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_call)) {
+int _BTreeDestructor__(BTree* b_tree DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_call)) {
     if (b_tree->head) {
         _DeleteNode__(b_tree->head DEBUG_CODE_ADD(, info_call));
     }
@@ -35,7 +37,7 @@ int _BTreeDestructor__(Btree* b_tree DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__
 
 Node* _CreateNode__(DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_call)) {
     Node* new_node = nullptr;
-    new_node = (Node*)calloc(1, sizeof(Node*));
+    new_node = (Node*)calloc(1, sizeof(Node));
     assert(new_node != nullptr);
 
     _InizializeNode__(new_node);
@@ -58,9 +60,59 @@ int _DeleteNode__(Node* node DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_ca
 int _InizializeNode__(Node* node) {
     assert(node != nullptr);
 
-    node->data  = "";
-    node->left  = nullptr;
+    node->left = nullptr;
     node->right = nullptr;
 
     return _ERROR_CODE_SUCCESSFULL__;
+}
+
+/*----------------------------------------------------------------------------------------------*/
+
+const char* LOG_FILENAME_VISUAL = "dump_doubly_link_list_visual.dot";
+const char* FILENAME_PNG = "dump_dlink_list_";
+
+const int SIZE_PNG_NAME = 40;
+const int SIZE_BUFFER_COMMAND_SYSTEM = 200;
+
+void bst_print_dot_null(char* key, int nullcount, FILE* stream) {
+    fprintf(stream, "    null%d [shape=point];\n", nullcount);
+    fprintf(stream, "    \"%s\" -> null%d;\n", key, nullcount);
+}
+
+void bst_print_dot_aux(Node* node, FILE* stream) {
+    static int nullcount = 0;
+
+    if (node->left)
+    {
+        fprintf(stream, "    \"%s\" -> \"%s\";\n", node->data, node->left->data);
+        bst_print_dot_aux(node->left, stream);
+    }
+    else
+        bst_print_dot_null(node->data, nullcount++, stream);
+
+    if (node->right)
+    {
+        fprintf(stream, "    \"%s\" -> \"%s\";\n", node->data, node->right->data);
+        bst_print_dot_aux(node->right, stream);
+    }
+    else
+        bst_print_dot_null(node->data, nullcount++, stream);
+}
+
+void bst_print_dot(Node* tree) {
+    FILE* graph_file = fopen(LOG_FILENAME_VISUAL, "w");
+
+    fprintf(graph_file, "digraph BST {\n");
+    fprintf(graph_file, "    node [fontname=\"Arial\"];\n");
+
+    if (!tree)
+        fprintf(graph_file, "\n");
+    else if (!tree->right && !tree->left)
+        fprintf(graph_file, "    %d;\n", tree->data);
+    else
+        bst_print_dot_aux(tree, graph_file);
+
+    fprintf(graph_file, "}\n");
+
+    fclose(graph_file);
 }
--- binary_tree/binary_tree.h
+++ binary_tree/binary_tree.h
@@ -3,7 +3,7 @@
 
 /*----------------------------------------------------------------------------------------------*/
 
-#include "../../bin/_mtLib/logger/logger.h"
+#include "../logger/logger.h"
 
 /*----------------------------------------------------------------------------------------------*/
 
@@ -11,19 +11,19 @@
 #define DEBUG_MODE_BTREE_OFF__  0
 
 #ifndef DEBUG_MODE_BTREE
-    #define DEBUG_MODE_BTREE DEBUG_MODE_BTREE_OFF__
+#define DEBUG_MODE_BTREE DEBUG_MODE_BTREE_OFF__
 #endif // !DEBUG_MODE_BTREE
 
 
 #if DEBUG_MODE_BTREE == DEBUG_MODE_BTREE_ON__
-    #define DEBUG_CODE_ADD(...) __VA_ARGS__
+#define DEBUG_CODE_ADD(...) __VA_ARGS__
 #else
-    #define DEBUG_CODE_ADD(...)
+#define DEBUG_CODE_ADD(...)
 #endif
 
 /*----------------------------------------------------------------------------------------------*/
 
-typedef const char* BTreeElem_t;
+typedef char* BTreeElem_t;
 
 struct Node {
     BTreeElem_t data;
@@ -31,20 +31,26 @@ struct Node {
     Node* right = nullptr;
 };
 
-struct Btree {
+struct BTree {
     Node* head;
 };
 
-#define BTreeConstructor(...)  _BTreeConstructor__(__VA_ARGS__)
-int     _BTreeConstructor__(Btree* b_tree DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_call));
+/*----------------------------------------------------------------------------------------------*/
+
+#define BTreeConstructor(...)  _BTreeConstructor__(__VA_ARGS__ DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__{LOCATION_VAR__(__VA_ARGS__)}))
+int     _BTreeConstructor__(BTree* b_tree DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_call));
 
 #define BTreeDestructor(...) _BTreeDestructor__(__VA_ARGS__);
-int     _BTreeDestructor__(Btree* b_tree DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_call));
+int     _BTreeDestructor__(BTree* b_tree DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_call));
 
 #define CreateNode(...) _CreateNode__(__VA_ARGS__)
 Node* _CreateNode__(DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_call));
 
 #define DeleteNode(...) _DeleteNode__(__VA_ARGS__)
 int _DeleteNode__(Node* node DEBUG_CODE_ADD(, LOCATION_VAR_CALL_STRUCT__ info_call));
+
+/*----------------------------------------------------------------------------------------------*/
+
+void bst_print_dot(Node* tree);
 
 #endif
